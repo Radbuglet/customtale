14b5287f1be20d92d070ef65ef415978a2d0f112
radbuglet <10578060+Radbuglet@users.noreply.github.com>
2026-01-19 22:06:40 -0700

    replace `surf` with `reqwest`

--- crates/customtale-auth/src/session_api.rs
+++ crates/customtale-auth/src/session_api.rs
@@ -11,184 +11,20 @@ const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VE
 #[derive(Debug, Error, Diagnostic)]
 pub enum SessionServiceError {
     #[error("failed to connect to session server")]
-    Connect(#[source] Box<dyn std::error::Error + 'static>),
+    Connect(#[source] reqwest::Error),
     #[error("failed to decode session server response")]
-    Body(#[source] Box<dyn std::error::Error + 'static>),
+    Body(#[source] reqwest::Error),
     #[error("session server responded with non-200 status {status}: {body}")]
     Status {
-        status: surf::StatusCode,
+        status: reqwest::StatusCode,
         body: String,
     },
-}
-
-pub async fn request_authorization_grant(
-    identity_token: &str,
-    server_audience: &str,
-    bearer_token: &str,
-) -> Result<String, SessionServiceError> {
-    #[derive(Serialize)]
-    struct RequestBody<'a> {
-        #[serde(rename = "identityToken")]
-        identity_token: &'a str,
-        #[serde(rename = "aud")]
-        server_audience: &'a str,
-    }
-
-    #[derive(Deserialize)]
-    struct ResponseBody {
-        #[serde(rename = "authorizationGrant")]
-        authorization_grant: String,
-    }
-
-    let mut resp = surf::post(format!("{SESSION_SERVER_URL}/server-join/auth-grant"))
-        .header("Content-Type", "application/json")
-        .header("Accept", "application/json")
-        .header("Authorization", format!("Bearer {bearer_token}"))
-        .header("User-Agent", USER_AGENT)
-        .body(
-            surf::Body::from_json(&RequestBody {
-                identity_token,
-                server_audience,
-            })
-            .unwrap(),
-        )
-        .send()
-        .await
-        .map_err(|err| SessionServiceError::Connect(err.into()))?;
-
-    if resp.status() != surf::StatusCode::Ok {
-        let (Ok(body) | Err(body)) = resp.body_string().await.map_err(|v| v.to_string());
-
-        return Err(SessionServiceError::Status {
-            status: resp.status(),
-            body,
-        });
-    }
-
-    let body = resp
-        .body_json::<ResponseBody>()
-        .await
-        .map_err(|err| SessionServiceError::Body(err.into()))?;
-
-    Ok(body.authorization_grant)
-}
-
-pub async fn exchange_auth_grant_for_token(
-    authorization_grant: &str,
-    x509_fingerprint: &str,
-    bearer_token: &str,
-) -> Result<String, SessionServiceError> {
-    #[derive(Serialize)]
-    struct RequestBody<'a> {
-        #[serde(rename = "authorizationGrant")]
-        authorization_grant: &'a str,
-        #[serde(rename = "x509Fingerprint")]
-        x509_fingerprint: &'a str,
-    }
-
-    #[derive(Deserialize)]
-    struct ResponseBody {
-        #[serde(rename = "accessToken")]
-        access_token: String,
-    }
-
-    let mut resp = surf::post(format!("{SESSION_SERVER_URL}/server-join/auth-token"))
-        .header("Content-Type", "application/json")
-        .header("Accept", "application/json")
-        .header("Authorization", format!("Bearer {bearer_token}"))
-        .header("User-Agent", USER_AGENT)
-        .body(
-            surf::Body::from_json(&RequestBody {
-                authorization_grant,
-                x509_fingerprint,
-            })
-            .unwrap(),
-        )
-        .send()
-        .await
-        .map_err(|err| SessionServiceError::Connect(err.into()))?;
-
-    if resp.status() != surf::StatusCode::Ok {
-        let (Ok(body) | Err(body)) = resp.body_string().await.map_err(|v| v.to_string());
-
-        return Err(SessionServiceError::Status {
-            status: resp.status(),
-            body,
-        });
-    }
-
-    let body = resp
-        .body_json::<ResponseBody>()
-        .await
-        .map_err(|err| SessionServiceError::Body(err.into()))?;
-
-    Ok(body.access_token)
-}
-
-pub async fn get_jwks() -> Result<jsonwebtoken::jwk::Jwk, SessionServiceError> {
-    let mut resp = surf::get(format!("{SESSION_SERVER_URL}/.well-known/jwks.json"))
-        .header("Accept", "application/json")
-        .header("User-Agent", USER_AGENT)
-        .send()
-        .await
-        .map_err(|err| SessionServiceError::Connect(err.into()))?;
-
-    if resp.status() != surf::StatusCode::Ok {
-        let (Ok(body) | Err(body)) = resp.body_string().await.map_err(|v| v.to_string());
-
-        return Err(SessionServiceError::Status {
-            status: resp.status(),
-            body,
-        });
-    }
-
-    let body = resp
-        .body_json::<jsonwebtoken::jwk::Jwk>()
-        .await
-        .map_err(|err| SessionServiceError::Body(err.into()))?;
-
-    Ok(body)
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct GameProfile {
     pub uuid: Uuid,
     pub username: String,
-}
-
-pub async fn get_game_profiles(
-    oauth_access_token: &str,
-) -> Result<Vec<GameProfile>, SessionServiceError> {
-    #[derive(Deserialize)]
-    struct LauncherDataResponse {
-        #[serde(rename = "owner")]
-        _owner: Uuid,
-        profiles: Vec<GameProfile>,
-    }
-
-    let mut resp = surf::get("https://account-data.hytale.com/my-account/get-profiles")
-        .header("Accept", "application/json")
-        .header("Authorization", format!("Bearer {oauth_access_token}"))
-        .header("User-Agent", USER_AGENT)
-        .send()
-        .await
-        .map_err(|err| SessionServiceError::Connect(err.into()))?;
-
-    if resp.status() != surf::StatusCode::Ok {
-        let (Ok(body) | Err(body)) = resp.body_string().await.map_err(|v| v.to_string());
-
-        return Err(SessionServiceError::Status {
-            status: resp.status(),
-            body,
-        });
-    }
-
-    let body = resp
-        .body_json::<LauncherDataResponse>()
-        .await
-        .map_err(|err| SessionServiceError::Body(err.into()))?;
-
-    Ok(body.profiles)
 }
 
 #[derive(Debug, Clone, Deserialize)]
@@ -201,86 +37,241 @@ pub struct GameSessionResponse {
     pub expires_at: String,
 }
 
-pub async fn create_game_session(
-    oauth_access_token: &str,
-    profile_uuid: Uuid,
-) -> Result<GameSessionResponse, SessionServiceError> {
-    #[derive(Serialize)]
-    struct RequestBody {
-        uuid: Uuid,
-    }
+// === SessionService === //
 
-    let mut resp = surf::post(format!("{SESSION_SERVER_URL}/game-session/new"))
-        .header("Content-Type", "application/json")
-        .header("Authorization", format!("Bearer {oauth_access_token}"))
-        .header("User-Agent", USER_AGENT)
-        .body(surf::Body::from_json(&RequestBody { uuid: profile_uuid }).unwrap())
-        .send()
-        .await
-        .map_err(|err| SessionServiceError::Connect(err.into()))?;
-
-    if resp.status() != surf::StatusCode::Ok {
-        let (Ok(body) | Err(body)) = resp.body_string().await.map_err(|v| v.to_string());
-
-        return Err(SessionServiceError::Status {
-            status: resp.status(),
-            body,
-        });
-    }
-
-    let body = resp
-        .body_json::<GameSessionResponse>()
-        .await
-        .map_err(|err| SessionServiceError::Body(err.into()))?;
-
-    Ok(body)
+#[derive(Debug)]
+pub struct SessionService {
+    client: reqwest::Client,
 }
 
-pub async fn refresh_session(
-    session_token: &str,
-) -> Result<GameSessionResponse, SessionServiceError> {
-    let mut resp = surf::post(format!("{SESSION_SERVER_URL}/game-session/new"))
-        .header("Content-Type", "application/json")
-        .header("Authorization", format!("Bearer {session_token}"))
-        .header("User-Agent", USER_AGENT)
-        .send()
-        .await
-        .map_err(|err| SessionServiceError::Connect(err.into()))?;
-
-    if resp.status() != surf::StatusCode::Ok {
-        let (Ok(body) | Err(body)) = resp.body_string().await.map_err(|v| v.to_string());
-
-        return Err(SessionServiceError::Status {
-            status: resp.status(),
-            body,
-        });
+impl SessionService {
+    pub fn new() -> Result<Self, reqwest::Error> {
+        Ok(Self {
+            client: reqwest::ClientBuilder::new().build()?,
+        })
     }
 
-    let body = resp
-        .body_json::<GameSessionResponse>()
-        .await
-        .map_err(|err| SessionServiceError::Body(err.into()))?;
+    pub async fn request_authorization_grant(
+        &self,
+        identity_token: &str,
+        server_audience: &str,
+        bearer_token: &str,
+    ) -> Result<String, SessionServiceError> {
+        #[derive(Serialize)]
+        struct RequestBody<'a> {
+            #[serde(rename = "identityToken")]
+            identity_token: &'a str,
+            #[serde(rename = "aud")]
+            server_audience: &'a str,
+        }
 
-    Ok(body)
+        #[derive(Deserialize)]
+        struct ResponseBody {
+            #[serde(rename = "authorizationGrant")]
+            authorization_grant: String,
+        }
+
+        let resp = self
+            .client
+            .post(format!("{SESSION_SERVER_URL}/server-join/auth-grant"))
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .header("Authorization", format!("Bearer {bearer_token}"))
+            .header("User-Agent", USER_AGENT)
+            .json(&RequestBody {
+                identity_token,
+                server_audience,
+            })
+            .send()
+            .await
+            .map_err(SessionServiceError::Connect)?;
+
+        let resp = filter_status(resp).await?;
+
+        let body = resp
+            .json::<ResponseBody>()
+            .await
+            .map_err(SessionServiceError::Body)?;
+
+        Ok(body.authorization_grant)
+    }
+
+    pub async fn exchange_auth_grant_for_token(
+        &self,
+        authorization_grant: &str,
+        x509_fingerprint: &str,
+        bearer_token: &str,
+    ) -> Result<String, SessionServiceError> {
+        #[derive(Serialize)]
+        struct RequestBody<'a> {
+            #[serde(rename = "authorizationGrant")]
+            authorization_grant: &'a str,
+            #[serde(rename = "x509Fingerprint")]
+            x509_fingerprint: &'a str,
+        }
+
+        #[derive(Deserialize)]
+        struct ResponseBody {
+            #[serde(rename = "accessToken")]
+            access_token: String,
+        }
+
+        let resp = self
+            .client
+            .post(format!("{SESSION_SERVER_URL}/server-join/auth-token"))
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .header("Authorization", format!("Bearer {bearer_token}"))
+            .header("User-Agent", USER_AGENT)
+            .json(&RequestBody {
+                authorization_grant,
+                x509_fingerprint,
+            })
+            .send()
+            .await
+            .map_err(SessionServiceError::Connect)?;
+
+        let resp = filter_status(resp).await?;
+
+        let body = resp
+            .json::<ResponseBody>()
+            .await
+            .map_err(SessionServiceError::Body)?;
+
+        Ok(body.access_token)
+    }
+
+    pub async fn get_jwks(&self) -> Result<jsonwebtoken::jwk::Jwk, SessionServiceError> {
+        let resp = self
+            .client
+            .get(format!("{SESSION_SERVER_URL}/.well-known/jwks.json"))
+            .header("Accept", "application/json")
+            .header("User-Agent", USER_AGENT)
+            .send()
+            .await
+            .map_err(SessionServiceError::Connect)?;
+
+        let resp = filter_status(resp).await?;
+
+        let body = resp
+            .json::<jsonwebtoken::jwk::Jwk>()
+            .await
+            .map_err(SessionServiceError::Body)?;
+
+        Ok(body)
+    }
+
+    pub async fn get_game_profiles(
+        &self,
+        oauth_access_token: &str,
+    ) -> Result<Vec<GameProfile>, SessionServiceError> {
+        #[derive(Deserialize)]
+        struct LauncherDataResponse {
+            #[serde(rename = "owner")]
+            _owner: Uuid,
+            profiles: Vec<GameProfile>,
+        }
+
+        let resp = self
+            .client
+            .get("https://account-data.hytale.com/my-account/get-profiles")
+            .header("Accept", "application/json")
+            .header("Authorization", format!("Bearer {oauth_access_token}"))
+            .header("User-Agent", USER_AGENT)
+            .send()
+            .await
+            .map_err(SessionServiceError::Connect)?;
+
+        let resp = filter_status(resp).await?;
+
+        let body = resp
+            .json::<LauncherDataResponse>()
+            .await
+            .map_err(SessionServiceError::Body)?;
+
+        Ok(body.profiles)
+    }
+
+    pub async fn create_game_session(
+        &self,
+        oauth_access_token: &str,
+        profile_uuid: Uuid,
+    ) -> Result<GameSessionResponse, SessionServiceError> {
+        #[derive(Serialize)]
+        struct RequestBody {
+            uuid: Uuid,
+        }
+
+        let resp = self
+            .client
+            .post(format!("{SESSION_SERVER_URL}/game-session/new"))
+            .header("Content-Type", "application/json")
+            .header("Authorization", format!("Bearer {oauth_access_token}"))
+            .header("User-Agent", USER_AGENT)
+            .json(&RequestBody { uuid: profile_uuid })
+            .send()
+            .await
+            .map_err(SessionServiceError::Connect)?;
+
+        let resp = filter_status(resp).await?;
+
+        let body = resp
+            .json::<GameSessionResponse>()
+            .await
+            .map_err(SessionServiceError::Body)?;
+
+        Ok(body)
+    }
+
+    pub async fn refresh_session(
+        &self,
+        session_token: &str,
+    ) -> Result<GameSessionResponse, SessionServiceError> {
+        let resp = self
+            .client
+            .post(format!("{SESSION_SERVER_URL}/game-session/new"))
+            .header("Content-Type", "application/json")
+            .header("Authorization", format!("Bearer {session_token}"))
+            .header("User-Agent", USER_AGENT)
+            .send()
+            .await
+            .map_err(SessionServiceError::Connect)?;
+
+        let resp = filter_status(resp).await?;
+
+        let body = resp
+            .json::<GameSessionResponse>()
+            .await
+            .map_err(SessionServiceError::Body)?;
+
+        Ok(body)
+    }
+
+    pub async fn terminate_session(&self, session_token: &str) -> Result<(), SessionServiceError> {
+        let resp = self
+            .client
+            .delete(format!("{SESSION_SERVER_URL}/game-session"))
+            .header("Content-Type", "application/json")
+            .header("Authorization", format!("Bearer {session_token}"))
+            .header("User-Agent", USER_AGENT)
+            .send()
+            .await
+            .map_err(SessionServiceError::Connect)?;
+
+        let _resp = filter_status(resp).await?;
+
+        Ok(())
+    }
 }
 
-pub async fn terminate_session(session_token: &str) -> Result<(), SessionServiceError> {
-    let mut resp = surf::delete(format!("{SESSION_SERVER_URL}/game-session"))
-        .header("Content-Type", "application/json")
-        .header("Authorization", format!("Bearer {session_token}"))
-        .header("User-Agent", USER_AGENT)
-        .send()
-        .await
-        .map_err(|err| SessionServiceError::Connect(err.into()))?;
+async fn filter_status(resp: reqwest::Response) -> Result<reqwest::Response, SessionServiceError> {
+    let status = resp.status();
 
-    if resp.status() != surf::StatusCode::Ok {
-        let (Ok(body) | Err(body)) = resp.body_string().await.map_err(|v| v.to_string());
+    if status != reqwest::StatusCode::OK {
+        let (Ok(body) | Err(body)) = resp.text().await.map_err(|v| v.to_string());
 
-        return Err(SessionServiceError::Status {
-            status: resp.status(),
-            body,
-        });
+        return Err(SessionServiceError::Status { status, body });
     }
 
-    Ok(())
+    Ok(resp)
 }
