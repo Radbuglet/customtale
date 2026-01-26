379a0c195644351e4b59951cd11f930e8c080c79
radbuglet <10578060+Radbuglet@users.noreply.github.com>
2026-01-25 18:50:40 -0700

    generate some packets

--- crates/customtale-protocol-tools/app/src/main/kotlin/net/coopfury/customtale_protocol_tools/App.kt
+++ crates/customtale-protocol-tools/app/src/main/kotlin/net/coopfury/customtale_protocol_tools/App.kt
@@ -1,16 +1,12 @@
 package net.coopfury.customtale_protocol_tools
 
-import java.lang.reflect.Constructor
-import java.lang.reflect.Modifier
-import java.lang.reflect.Parameter
-import java.lang.reflect.ParameterizedType
-import java.lang.reflect.Type
+import io.netty.buffer.Unpooled
+import java.lang.reflect.*
 import java.net.URL
 import java.net.URLClassLoader
 import java.nio.file.Path
-import java.util.UUID
+import java.util.*
 import kotlin.random.Random
-import io.netty.buffer.Unpooled
 
 fun main(args: Array<String>) {
     if (args.size != 1)
@@ -32,138 +28,176 @@ fun main(args: Array<String>) {
 
         println(packet.name)
 
-        val packetInstance = randomizeInstance(packet, Random.Default, 0)
+        val packetInstance = randomizeInstance(packet, Random.Default) ?: continue
         val outBuf = Unpooled.buffer()
         serializeMethod.invoke(packetInstance, outBuf)
-        println(outBuf)
+
+        val outBufRaw = ByteArray(outBuf.readableBytes())
+        outBuf.readBytes(outBufRaw)
+
+        println(formatByteArray(outBufRaw))
     }
 }
 
-fun randomLenForDepth(rng: Random, depth: Int) : Int {
-    if (depth > 8)
-        return 0
+fun randomizeInstance(ty: Class<*>, rng: Random) : Any? {
+    val gen = RandomInstanceGenerator(rng)
+    val instance = gen.randomize(ty, 0)
 
-    return rng.nextInt(8)
-}
-
-fun randomizeInstance(ty: Parameter, rng: Random, depth: Int) : Any? {
-    var isNullable = false
-    for (anno in ty.annotations)
-        if (anno.annotationClass.simpleName == "Nullable")
-            isNullable = true
-
-    if (isNullable && rng.nextBoolean())
+    if (gen.tainted)
         return null
 
-    return randomizeInstance(ty.parameterizedType, rng, depth)
+    return instance
 }
 
-fun randomizeInstance(ty: Type, rng: Random, depth: Int) : Any? {
-    if (ty is ParameterizedType)
-        return randomizeInstance(ty, rng, depth)
+fun formatByteArray(arr: ByteArray) : String {
+    val builder = StringBuilder()
+    var first = true
 
-    return randomizeInstance(ty as Class<*>, rng, depth)
-}
+    builder.append("[")
 
-fun randomizeInstance(ty: ParameterizedType, rng: Random, depth: Int) : Any? {
-    val args = ty.actualTypeArguments
-    if (args.isEmpty())
-        return randomizeInstance(ty.rawType, rng, depth)
-
-    if (ty.rawType == Map::class.java) {
-        val len = randomLenForDepth(rng, depth)
-        val map = mutableMapOf<Any?, Any?>()
-
-        (0..<len).forEach { _ ->
-            map[randomizeInstance(args[0], rng, depth + 1)] = randomizeInstance(args[1], rng, depth + 1)
+    for (elem in arr) {
+        if (!first) {
+            builder.append(", ")
         }
-
-        return map
+        builder.append(elem.toUByte())
+        first = false
     }
 
-    throw UnsupportedOperationException("unknown parameterized class type $ty")
+    builder.append("]")
+
+    return builder.toString()
 }
 
-@Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
-fun randomizeInstance(ty: Class<*>, rng: Random, depth: Int) : Any? {
-    if (ty == Boolean::class.java || ty == java.lang.Boolean::class.java)
-        return rng.nextBoolean()
+class RandomInstanceGenerator(val rng: Random) {
+    var tainted: Boolean = false
 
-    if (ty == Byte::class.java || ty == java.lang.Byte::class.java)
-        return rng.nextInt().toByte()
+    fun randomLenForDepth(depth: Int) : Int {
+        if (depth > 8)
+            return 0
 
-    if (ty == Short::class.java || ty == java.lang.Short::class.java)
-        return rng.nextInt().toShort()
-
-    if (ty == Int::class.java || ty == Integer::class.java)
-        return rng.nextInt()
-
-    if (ty == Long::class.java)
-        return rng.nextLong()
-
-    if (ty == Float::class.java)
-        return rng.nextFloat()
-
-    if (ty == Double::class.java)
-        return rng.nextDouble()
-
-    if (ty == String::class.java)
-        return rng.nextInt().toString()
-
-    if (ty == UUID::class.java)
-        return UUID.randomUUID()
-
-    if (ty.isArray) {
-        val len = randomLenForDepth(rng, depth)
-        val arr = java.lang.reflect.Array.newInstance(ty.componentType, len)
-        (0..<len).forEach { i ->
-            java.lang.reflect.Array.set(arr, i, randomizeInstance(ty.componentType, rng, depth + 1))
-        }
-
-        return arr
+        return rng.nextInt(8)
     }
 
-    if (ty.name.startsWith("com.hypixel.hytale.protocol")) {
-        if (ty.isEnum) {
-            val variants = ty.getField("VALUES").get(null) as Array<*>
-            return variants[rng.nextInt(variants.size)]
-        } else if (Modifier.isAbstract(ty.modifiers)) {
-            // TODO
+    fun randomize(ty: Parameter, depth: Int) : Any? {
+        var isNullable = false
+        for (anno in ty.annotations)
+            if (anno.annotationClass.simpleName == "Nullable")
+                isNullable = true
+
+        if (isNullable && rng.nextBoolean())
             return null
-        } else {
-            return randomizeInstanceHytaleStruct(ty, rng, depth)
-        }
+
+        return randomize(ty.parameterizedType, depth)
     }
 
-    throw UnsupportedOperationException("unknown unparameterized class type $ty")
-}
+    fun randomize(ty: Type, depth: Int) : Any? {
+        if (ty is ParameterizedType)
+            return randomize(ty, depth)
 
-fun randomizeInstanceHytaleStruct(ty: Class<*>, rng: Random, depth: Int) : Any? {
-    var emptyCtor = null as Constructor<*>?
-
-    for (ctor in ty.constructors) {
-        val params = ctor.parameters
-
-        if (params.size == 0) {
-            emptyCtor = ctor
-            continue
-        }
-
-        if (params.size == 1 && params[0].type == ty)
-            continue
-
-        val args = mutableListOf<Any?>()
-
-        for (param in params) {
-            args += randomizeInstance(param, rng, depth + 1)
-        }
-
-        return ctor.newInstance(*args.toTypedArray())
+        return randomize(ty as Class<*>, depth)
     }
 
-    if (emptyCtor != null) {
-        return emptyCtor.newInstance()
+    fun randomize(ty: ParameterizedType, depth: Int) : Any? {
+        val args = ty.actualTypeArguments
+        if (args.isEmpty())
+            return randomize(ty.rawType, depth)
+
+        if (ty.rawType == Map::class.java) {
+            val len = randomLenForDepth(depth)
+            val map = mutableMapOf<Any?, Any?>()
+
+            (0..<len).forEach { _ ->
+                map[randomize(args[0], depth + 1)] = randomize(args[1], depth + 1)
+            }
+
+            return map
+        }
+
+        throw UnsupportedOperationException("unknown parameterized class type $ty")
     }
 
-    throw UnsupportedOperationException("missing constructor for $ty")
+    @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
+    fun randomize(ty: Class<*>, depth: Int) : Any? {
+        if (ty == Boolean::class.java || ty == java.lang.Boolean::class.java)
+            return rng.nextBoolean()
+
+        if (ty == Byte::class.java || ty == java.lang.Byte::class.java)
+            return rng.nextInt().toByte()
+
+        if (ty == Short::class.java || ty == java.lang.Short::class.java)
+            return rng.nextInt().toShort()
+
+        if (ty == Int::class.java || ty == Integer::class.java)
+            return rng.nextInt()
+
+        if (ty == Long::class.java || ty == java.lang.Long::class.java)
+            return rng.nextLong()
+
+        if (ty == Float::class.java || ty == java.lang.Float::class.java)
+            return rng.nextFloat()
+
+        if (ty == Double::class.java  || ty == java.lang.Double::class.java)
+            return rng.nextDouble()
+
+        if (ty == String::class.java)
+            return rng.nextInt().toString()
+
+        if (ty == UUID::class.java)
+            return UUID.randomUUID()
+
+        if (ty.isArray) {
+            val len = randomLenForDepth(depth)
+            val arr = java.lang.reflect.Array.newInstance(ty.componentType, len)
+            (0..<len).forEach { i ->
+                java.lang.reflect.Array.set(arr, i, randomize(ty.componentType, depth + 1))
+            }
+
+            return arr
+        }
+
+        if (ty.name.startsWith("com.hypixel.hytale.protocol")) {
+            if (ty.isEnum) {
+                val variants = ty.getField("VALUES").get(null) as Array<*>
+                return variants[rng.nextInt(variants.size)]
+            } else if (Modifier.isAbstract(ty.modifiers)) {
+                tainted = true
+                return null
+            } else {
+                return randomizeInstanceHytaleStruct(ty, depth)
+            }
+        }
+
+        throw UnsupportedOperationException("unknown unparameterized class type $ty")
+    }
+
+    fun randomizeInstanceHytaleStruct(ty: Class<*>, depth: Int) : Any? {
+        var emptyCtor = null as Constructor<*>?
+
+        for (ctor in ty.constructors) {
+            val params = ctor.parameters
+
+            if (params.size == 0) {
+                emptyCtor = ctor
+                continue
+            }
+
+            if (params.size == 1 && params[0].type == ty)
+                continue
+
+            val args = mutableListOf<Any?>()
+
+            for (param in params) {
+                args += randomize(param, depth + 1)
+            }
+
+            return ctor.newInstance(*args.toTypedArray())
+        }
+
+        if (emptyCtor != null) {
+            return emptyCtor.newInstance()
+        }
+
+        throw UnsupportedOperationException("missing constructor for $ty")
+    }
+
 }
